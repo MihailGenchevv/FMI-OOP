48b424c6e7e9ee8b52bef200054027a822e29e92
MihailGenchev <mihail@investotech.com>
2024-03-30 14:16:59 +0200

    added praktikum exercise

---
+++ week10/array/main.cpp
@@ -0,0 +1,69 @@
+#include <vector>
+#include <iostream>
+
+
+class Assignment {
+public:
+    virtual int getPoints() const = 0;
+    virtual ~Assignment() = 0;
+};
+
+Assignment::~Assignment() {
+    //delete dynamic memory
+}
+
+class Homework1: public Assignment {
+    int task1;
+public:
+    Homework1(int points ){
+        task1= points;
+    }
+
+    int getPoints() const {
+        return task1;
+    }
+};
+
+
+class Homework2: public Assignment {
+    std::vector<int> points;
+public:
+    Homework2(int task1Points, int task2Points) {
+        points.push_back(task1Points);
+        points.push_back(task2Points);
+    }
+
+    int getPoints() const {
+        int total = 0;
+        for (auto x : points) {
+            total += x;  
+        }
+        return total;
+    }
+};
+
+
+int main() {
+
+    Assignment * hw1 = new Homework1(1);
+    Assignment * hw2 = new Homework2(4,2);
+
+    std::vector<Assignment *> assignments;
+
+    assignments.push_back(hw1);
+    assignments.push_back(hw2);
+
+    for (auto ptr : assignments){
+        std::cout << ptr->getPoints() << std::endl;
+    }
+
+    for (auto ptr : assignments) {
+        delete[] ptr;
+    }
+
+
+    // Assignment* * assin = new Assignment*[10];
+    // assin[0] = hw1;
+    // assin[1] = hw2;
+    // delete[] assin;
+}
